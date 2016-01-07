435b216c7f210ecbfaaea4cdef58fc106fd107bc
Dong Jin Du <arvect@users.noreply.github.com>
2016-01-06 23:46:16 -0500

    Update DBAgent.java

--- dongjindu/dbagent/DBAgent.java
+++ dongjindu/dbagent/DBAgent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 oefish.
+ * Copyright 2015 Dong Jin Du.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,15 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.dongjindu.dbagent;
+package dongjindu.dbagent;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author oefish
- */
 public interface DBAgent<T> {
     int STRINGDEFAULTLENGTH = 30;
 
